cfbadbe61aeec6db60c5535ff00ef538ec2f5f68
Dario0122 <dario.di.cuia@gmail.com>
2021-10-22 15:57:34 +0200

    d-block

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const menuHamburger = document.querySelector('.hamburger-menu')
+const menuHamburger = document .querySelector('.hamburger-menu')
+console.log('click');
 
 const hamburger = document .querySelector('.fa-bars')
 
 hamburger.addEventListener ('click', function() {
     console.log('click');
+
+    menuHamburger.classList.add(d-block)
+    
 });
